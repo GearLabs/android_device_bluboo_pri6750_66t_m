0dd4e0984bf756ddc242687b26a63184391d1f03
Alberto Guanti <albertoguanti@gmail.com>
2016-12-23 22:19:37 +0100

    Revert "porridge: Fork handheld_core_hardware"
    * we have a compass here

--- device.mk
+++ device.mk
@@ -37,7 +37,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
     frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
-    $(LOCAL_PATH)/configs/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
 # Media
 PRODUCT_COPY_FILES += \
