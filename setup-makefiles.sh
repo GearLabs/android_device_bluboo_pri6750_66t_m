8c1c2a223edf7bdfb6335e54ba79aed42e9cfa74
Alberto Guanti <albertoguanti@gmail.com>
2017-06-08 22:03:49 +0200

    pri6750_66t_m: make libdpframework a proprietary module
    * required for compatibility with upcoming patches

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -188,6 +188,26 @@ LOCAL_MULTILIB := 64
 LOCAL_SRC_FILES_64 := proprietary/lib64/libaudiopolicymanager.so
 include \$(BUILD_PREBUILT)
 
+include \$(CLEAR_VARS)
+LOCAL_MODULE = libdpframework
+LOCAL_MODULE_OWNER := $VENDOR
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MULTILIB := 32
+LOCAL_SRC_FILES_32 := proprietary/lib/libdpframework.so
+include \$(BUILD_PREBUILT)
+
+include \$(CLEAR_VARS)
+LOCAL_MODULE = libdpframework
+LOCAL_MODULE_OWNER := $VENDOR
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MULTILIB := 64
+LOCAL_SRC_FILES_64 := proprietary/lib64/libdpframework.so
+include \$(BUILD_PREBUILT)
+
 endif
 endif
 
